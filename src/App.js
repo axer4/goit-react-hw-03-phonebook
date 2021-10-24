d2b1bf6e11c460b7fdffd9bdae222bf6c32f7e0b
axer4 <79149727+axer4@users.noreply.github.com>
2021-10-24 20:28:26 +0300

    bugfix

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import styles from './App.module.css'
 export default class PhoneBook extends Component {
  state = {
   contacts: [
-    // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ],
   filter: '',
 }
@@ -65,9 +65,9 @@ export default class PhoneBook extends Component {
   }
   render() {
     // const { contacts, filter } = this.state;
-      const filteredContacts = this.state.contacts.filter(contact => {
-        return contact.name.toLowerCase().includes(this.state.filter.toLowerCase());
-      })
+        const filteredContacts = this.state.contacts.filter(contact => {
+          return contact.name.toLowerCase().includes(this.state.filter.toLowerCase());
+        })
     
     // console.log(filteredContacts);
     return <>
